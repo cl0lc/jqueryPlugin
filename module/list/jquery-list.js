4ed44e0b77fb9ac4b18295e475b37938a6a93318
chenLong <1182927503@qq.com>
2019-03-28 09:58:58 +0800

    '更改配置为对象，添加removeNul、nullText配置选项'

--- module/list/jquery-list.js
+++ module/list/jquery-list.js
@@ -1,44 +1,56 @@
 /**
- * 更具data-name属性填充列表
- * @param  {Object}   data [列表数据]
- * @param  {Function} fn   [其他操作]
+ * 填充列表
+ * @param  {Object} opts [配置]
+ *                       opts.data 			Array		数据
+ *                       opts.fn 			Function    处理每一条数据时的其它操作，传递一条数据
+ *                       opts.removeNull	Boolean		是否移除空值dom的父元素，默认为false。(ps：当使用nullText时，该值必须为false)
+ *                       opts.nullText		String 		当值为'',null,undefined时。显示的替换文本，默认为空。
+ * @return {[type]}      [description]
  */
-$.fn.list = function(data, fn){
-	var list = $(this);
-	var tpl = list.find('.tpl');
-
-	list.empty().append(tpl);
-	// 遍历列表数据
-	for(var k = 0, l = data.length; k < l; k++){
-		var liData = data[k];
-		var li = tpl.clone().removeAttr('class');
-		var elems = li.find('[data-name]');
-		// 遍历要填充的dom
-		for(var i = 0, j = elems.length; i < j; i++){
-			var elem = elems.eq(i);
-			var key = elem.attr('data-name');
-			var val = '';
-			// 当值为多层嵌套时
-			if(key.indexOf(' ')  !== -1){
-				var keyArr = key.split(' ');
-				val = liData[keyArr[0]];
-				for(var n = 1, m = keyArr.length; n < m; n++){
-					val = val[keyArr[n]];
-				}
-			}else{
-				val = liData[key];
-			}
-			// 如果值为空，去掉改行
-			if(val === '' || val === undefined){
-				elem.parent().remove();
-			}else{
-				elems[i].innerHTML = val;
-			}
-		}
-		list.append(li);
-		// 填充列表时，其他操作
-		if(fn){
-			fn(liData);
-		}
+$.fn.list = function(opts) {
+	var def = {
+		removeNull: false,
+		nullText: '空'
 	}
+	var config = $.extend({}, def, opts);
+	return this.each(function(index, el) {
+		var _this = el,
+			$this = $(el),
+			tpl = $this.find('.tpl'); //列表模板
+		$this.empty().append(tpl);
+		// 遍历列表数据
+		for (var i = 0, j = data.length; i < j; i++) {
+			var liData = data[i],
+				li = tpl.clone().removeAttr('class'),
+				elems = li.find('[data-name]');
+			// 遍历需要填充数据的dom
+			for (var n = 0, m = elems.length; n < m; n++) {
+				var elem = elems.eq(n),
+					keyArr = elem.attr('data-name').split(' '),
+					val = liData[keyArr[0]];
+				// 多层嵌套数据
+				if (keyArr.length > 1) {
+					// 遍历多层嵌套数据
+					for (var k = 1, l = keyArr.length; k < l; k++) {
+						val = val[keyArr[k]];
+					}
+				}
+				if (val === '' || val === null || val === undefined) {
+					if(config.removeNull){
+						elem.parent().remove();
+					}else{
+						val = config.nullText;
+						elem.html(val);
+					}
+				} else {
+					elem.html(val);
+				}
+			}
+			$this.append(li);
+			// 填充列表时，其他操作
+			if (config.fn) {
+				config.fn(liData);
+			}
+		}
+	});
 }
